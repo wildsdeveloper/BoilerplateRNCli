72454936d6deb78193b3f84f801c6643fcfaaae6
Wilds Rincon <wildsrincon.developer@gmail.com>
2024-05-05 17:02:33 -0500

    feat: initial config boilerplate eslint and prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,14 @@
 module.exports = {
-  arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  printWidth: 100,
+  endOfLine: 'auto',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  useTabs: false,
+  tabWidth: 2,
+  importOrderSortSpecifiers: true,
+  importOrderSeparation: true,
+  importOrder: ['^react', '<THIRD_PARTY_MODULES>', '^../(.*)$', '^[./]'],
 };
